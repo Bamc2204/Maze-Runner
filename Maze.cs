68538f32725dce0bbfff701883f5e0c506ce4b8b
Bamc2204 <bryan.mora2204@gmail.com>
2025-01-02 00:53:18 -0500

    ninth commit

--- Maze.cs
+++ Maze.cs
@@ -23,7 +23,7 @@ class Maze
         _rows = (rows % 2 == 0) ? rows + 1 : rows;
         _cols = (cols % 2 == 0) ? cols + 1 : cols;
 
-        _maze = new int[_cols, _rows];
+        _maze = new int[_rows, _cols];
         _initializeMaze(); //Crea el laberinto pero vacio(con todo paredes);
         _generateMaze(1, 1); // Comenzar desde la celda (1,1) a construir el laberinto;
         _setEntryExit(); //Crear la entrada/salida del laberinto;
@@ -34,9 +34,9 @@ class Maze
     // Inicializa el laberinto con paredes (1) por defecto
     private void _initializeMaze()
     {
-        for (int x = 0; x < _cols; x++)
+        for (int x = 0; x < _rows; x++)
         {
-            for (int y = 0; y < _rows; y++)
+            for (int y = 0; y < _cols; y++)
             {
                 _maze[x, y] = 1; // Coloca paredes por defecto
             }
@@ -46,7 +46,7 @@ class Maze
     // Genera el laberinto utilizando el algoritmo de backtracking
     private void _generateMaze(int x, int y)
     {
-        _maze[y, x] = 0; // Marca la celda actual como un camino vacío
+        _maze[x, y] = 0; // Marca la celda actual como un camino vacío
         // Lista de direcciones aleatorias para explorar
         int[] addresses = {0, 1, 2, 3};
         addresses = _mess(addresses); // Aleatoriza las direcciones{abajo, derecha, arriba, izquierda}
@@ -61,13 +61,13 @@ class Maze
             if (_isValid(nx, ny))
             {
                 // Rompemos el muro entre la celda actual y la nueva celda
-                _maze[y + DirY[address], x + DirX[address]] = 0;
+                _maze[x + DirX[address], y + DirY[address]] = 0;
                 // Llamamos recursivamente para continuar el recorrido
                 _generateMaze(nx, ny);
             }
         }
     }
-
+ 
     //Desordenar Array
     private int[] _mess(int[] a)
     {
@@ -83,14 +83,14 @@ class Maze
     // Verifica si una celda es válida para colocar un camino*******
     private bool _isValid(int x, int y)
     {
-        return x > 0 && y > 0 && x < _rows - 1 && y < _cols - 1 && _maze[y, x] == 1;
+        return x > 0 && y > 0 && x < _rows - 1 && y < _cols - 1 && _maze[x, y] == 1;
     }
 
     // Establece la entrada y salida del laberinto
     private void _setEntryExit()
     {
         _maze[1, 0] = 0; // Entrada (cerca de la esquina superior izquierda)
-        _maze[_cols - 2, _rows - 1] = 0; // Salida (cerca de la esquina inferior derecha)
+        _maze[_rows - 2, _cols - 1] = 0; // Salida (cerca de la esquina inferior derecha)
     }
 
     // Establece el jugador
@@ -102,24 +102,46 @@ class Maze
     // Establece Trampas
     private void _setTraps()
     {
-        for(int i = 0; i < 20; i++) 
+        int cont = 0;
+        while(cont < 16)
         {
-            int x = _random.Next(5, _rows - 3);
-            int y = _random.Next(5, _cols - 3);
-            if(!((_maze[y + 1, x] == 1 && _maze[y - 1, x] == 0) || (_maze[y + 1, x] == 0 && _maze[y - 1, x] == 1) || 
-            (_maze[y, x + 1] == 1 && _maze[y, x - 1] == 0) || (_maze[y, x + 1] == 0 && _maze[y, x - 1] == 1) || 
-            ((y == _maze.GetLength(0) - 2) && (x == _maze.GetLength(1) - 1))))
-                _maze [y, x] = 3;
+            //posiciones en el centro del mapa
+            int x = _random.Next(5, _rows - 5); 
+            int y = _random.Next(5, _cols - 5);
+            //Colocar trampas
+            if(_validTrap(x, y))
+            {
+                _maze[x, y] = 3;
+                cont++;
+            }
+            System.Console.WriteLine(_rows + " " + _maze.GetLength(0) + " " + _cols + " " + _maze.GetLength(1));
         }
+    }
+
+    // Verifica la posicion de la trampa
+    private bool _validTrap(int x, int y)
+    {
+        for(int i = 0; i < 3; i++) 
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                //Da una Exception de rango *******************************
+                //Verifica si se puede colocar trampas
+                if((_maze[x + i, y + j] == 3) || (_maze[x - i, y - j] == 3) || (_maze[x + i, y - j] == 3) || (_maze[x - i, y + j] == 3) 
+                || (!((_maze[(x+1), y] == 0 && _maze[(x-1), y] == 0) || (_maze[x, (y+1)] == 0 && _maze[x, (y-1)] == 0))))
+                    return false;
+            }    
+        }
+        return true;
     }
 
     // Muestra el laberinto en la consola
     public void PrintMaze()
     {
         Console.Clear(); //Limpia la consola
-        for (int x = 0; x < _cols; x++)
+        for (int x = 0; x < _rows; x++)
         {
-            for (int y = 0; y < _rows; y++)
+            for (int y = 0; y < _cols; y++)
             {
                 Console.Write(_maze[x, y] == 1 ? "██" : _maze[x, y] == 2 ? "PP" : _maze[x, y] == 3 ? "TT" : "  "); // Paredes representadas por '██' y caminos por espacios
             }
